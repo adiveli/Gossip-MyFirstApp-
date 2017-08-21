5185609847421f49810b47af4d8a096cab1d68b1
adiveli <adi.veli@ymail.com>
2017-08-21 19:45:54 +0300

    Initial Commit

---
+++ Gossip/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Gossip
+//
+//  Created by Adi Veliman on 21/08/2017.
+//  Copyright © 2017 Adi Veliman. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
